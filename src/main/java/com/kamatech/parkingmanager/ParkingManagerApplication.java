4e160b225289e4eda783dc3fa597ca88b4b87cde
fkamag <fabiokamagian@gmail.com>
2024-03-03 19:13:17 -0300

    feat Spring Security

--- src/main/java/com/kamatech/parkingmanager/ParkingManagerApplication.java
+++ src/main/java/com/kamatech/parkingmanager/ParkingManagerApplication.java
@@ -2,20 +2,12 @@ package com.kamatech.parkingmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class ParkingManagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParkingManagerApplication.class, args);
-	}
-
-	@GetMapping("/")
-	public String index() {
-		return "Iniciando projeto Parking Manager";
 	}
 
 }
